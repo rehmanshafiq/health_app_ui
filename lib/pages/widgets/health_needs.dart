e2e64c4fd5d8d5db9fb332c1cd6fca1077c174fd
Shafiq <shafiq.rehman@nextgeni.com>
2025-05-08 12:29:48 +0500

    Health needs list's item separated.

--- lib/pages/widgets/health_needs.dart
+++ lib/pages/widgets/health_needs.dart
@@ -20,28 +20,32 @@ class HealthNeeds extends StatelessWidget {
         children: List.generate(customIcons.length, (index) {
           return Padding(
             padding: EdgeInsets.only(right: 14.0, left: index == 0 ? 0.0 : 14.0,),
-            child: Column(
-              children: [
-                Container(
-                  width: 60,
-                  height: 60,
-                  padding: const EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Theme.of(context)
-                        .colorScheme
-                        .primary
-                        .withValues(alpha: 0.1),
-                  ),
-                  child: Image.asset(customIcons[index].icon),
-                ),
-                const SizedBox(height: 6),
-                Text(customIcons[index].name),
-              ],
-            ),
+            child: healthNeedItem(context, customIcons[index]),
           );
         }),
       ),
+    );
+  }
+
+  Widget healthNeedItem(BuildContext context, CustomIcon customIcon) {
+    return Column(
+      children: [
+        Container(
+          width: 60,
+          height: 60,
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Theme.of(context)
+                .colorScheme
+                .primary
+                .withValues(alpha: 0.1),
+          ),
+          child: Image.asset(customIcon.icon),
+        ),
+        const SizedBox(height: 6),
+        Text(customIcon.name),
+      ],
     );
   }
 }
